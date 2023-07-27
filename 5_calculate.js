f6a55c8a28bbc921a91c4e7d37684509288c576d
saimun5656 <shaounlosted@gmail.com>
2023-07-27 23:12:41 +0600

    4th problem

---
+++ 5_calculate.js
@@ -0,0 +1,20 @@
+function calculator(n1, n2, operator) {
+    if (operator === '+') {
+        return n1 + n2
+    }
+    else if (operator === '-') {
+        return n1 - n2
+    }
+
+    else if (operator === '*') {
+        return n1 * n2
+    }
+    else if (operator === '/') {
+        if (n2 === 0) {
+            return 'Division by zero is not allowed'
+        }
+        return n1 / n2
+    }
+}
+const result = calculator(15,3,'-')
+console.log(result);
